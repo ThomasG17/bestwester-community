56f6629956f8358c95143d4ceb743fd8f432ff96
Amélie <amliebarre@gmail.com>
2016-03-08 15:27:43 +0100

    ajout de la nouvelle architecture mvc

--- views/Exampleuser/userTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-var_dump($test);
-echo $test["user_id"];
-echo"<br>";
-echo $test["user_name"];
-echo"<br>";
-echo $test["serie_name"];
--- views/layout.php
+++ views/layout.php
@@ -1,13 +1,57 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Bestwest</title>
-        <meta name="description" content="Ma description">
-        <link type="text/css" rel="stylesheet" href="/css/styles.css" />
-        <link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-      Hellow!
-    </body>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="/web/css/bootstrap.min.css">
+    <link href="/web/css/cover.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="/web/css/style.css">
+    <title>Mon titre</title>
+</head>
+
+<body cz-shortcut-listen="true">
+
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Facebook</h3>
+              <nav>
+
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="/">Accueil</a></li>
+                  <li><a href="/vote">Voter</a></li>
+                  <li><a href="/play">S'inscrire</a></li>
+                  <li><a href="#">Nous contacter</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+
+		<?php include $this->view;?>
+   	      
+
+          <div class="mastfoot">
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    
+    <script src="/web/js/jquery-2.1.3.min.js"></script>
+    <script src="/web/js/bootstrap.min.js"></script>
+  
+
+  </body>
 </html>
