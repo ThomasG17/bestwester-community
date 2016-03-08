ea4a514623cf6d9eecd65a08c5b2fa6919ae9782
Amélie <amliebarre@gmail.com>
2016-03-08 17:09:11 +0100

    ajout layout > création header et background body

--- views/layout.php
+++ views/layout.php
@@ -5,53 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="/web/css/bootstrap.min.css">
-    <link href="/web/css/cover.css" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/bootstrap.min.css">
+    <link href="/public/css/main.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="/web/css/style.css">
-    <title>Mon titre</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+
+    <title>Best Western Community</title>
 </head>
 
-<body cz-shortcut-listen="true">
+<body>
 
+    <div class="container-fluid">
 
-    <div class="site-wrapper">
-
-      <div class="site-wrapper-inner">
-
-        <div class="cover-container">
-
-          <div class="masthead clearfix">
-            <div class="inner">
-              <h3 class="masthead-brand">Facebook</h3>
-              <nav>
-
-                <ul class="nav masthead-nav">
-                  <li class="active"><a href="/">Accueil</a></li>
-                  <li><a href="/vote">Voter</a></li>
-                  <li><a href="/play">S'inscrire</a></li>
-                  <li><a href="#">Nous contacter</a></li>
-                </ul>
-              </nav>
+        <div id="header">
+            <div class="header-brand"></div>
+            <div class="user-informations">
+                <p class="name">Nom prénom</p>
+                <p class="statut">Fonction</p>
+                <div class="hostel">
+                    <p class="hostel-name">Hôtel Marais Bastille</p>
+                    <p class="hostel-adress">36 Bd Richard Lenoir <br /> 75011 Paris</p>
+                </div>
             </div>
-          </div>
 
-
-		<?php include $this->view;?>
-   	      
-
-          <div class="mastfoot">
-          </div>
-
+            <nav id="main-nav">
+                <ul>
+                    <li><a href="javascript:void(0);" title="Accueil">Accueil</a></li>
+                    <li><a href="javascript:void(0);" title="Messagerie">Messagerie</a></li>
+                    <li><a href="javascript:void(0);" title="Planning">Planning</a></li>
+                    <li><a href="javascript:void(0);" title="Événements">Événements</a></li>
+                </ul>
+            </nav>
         </div>
 
-      </div>
+		<?php include $this->view;?>
 
     </div>
     
-    <script src="/web/js/jquery-2.1.3.min.js"></script>
-    <script src="/web/js/bootstrap.min.js"></script>
-  
+    <script src="/public/js/jquery-2.1.3.min.js"></script>
+    <script src="/public/js/bootstrap.min.js"></script>
 
   </body>
 </html>
