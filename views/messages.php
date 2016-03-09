70032a648e9a8995ffabc839b715984c37e6c55d
Amélie <amliebarre@gmail.com>
2016-03-09 23:12:47 +0100

    finalisation view messages

--- views/messages.php
+++ views/messages.php
@@ -1,21 +1,47 @@
-<div id="mail-box">
-	<div class="row">
-		<section id="today" class="col-md-10 col-md-offset-1">
+<div id="mail-box" class="row">
+	<section id="today" class="col-md-10 col-md-offset-1">
+		<div class="messages-header">
+			<h3 class="col-md-10">Reçus aujourd'hui</h3>
+			<div class="trash col-md-2">
+				<span><i class="fa fa-trash"></i></span>
+			</div>
+		</div>
+		<div class="box-content">
 			<div class="message">
 				<span class="mail-open col-md-1">Mail icon</span>
-				<div class="subject col-md-7">RE: Liste hôtels Paris</div>
-				<div class="from col-md-3">De : Best Western Londres <br /> le 08/03 à 08h00</div>
+				<div class="subject col-md-6">RE: Liste hôtels Paris</div>
+				<div class="from col-md-4">De : Best Western Londres <br /> le 08/03 à 08h00</div>
+				<form action="" class="delete-message col-md-1">
+					<input type="checkbox" name="delete-message" />
+				</form>
 			</div>
 			<div class="message">
 				<span class="mail-open col-md-1">Mail icon</span>
-				<div class="subject col-md-7">RE: Liste hôtels Paris</div>
-				<div class="from col-md-3">De : Best Western Londres <br /> le 08/03 à 08h00</div>
+				<div class="subject col-md-6">RE: Liste hôtels Paris</div>
+				<div class="from col-md-4">De : Best Western Londres <br /> le 08/03 à 08h00</div>
+				<form action="" class="delete-message col-md-1">
+					<input type="checkbox" name="delete-message" />
+				</form>
 			</div>
-		</section>
-	</div>
-	<div class="row">
-		<section id="previous">
+		</div>
+	</section>
 
-		</section>
-	</div>
+	<section id="previous" class="col-md-10 col-md-offset-1">
+		<div class="messages-header">
+			<h3 class="col-md-10">Reçus précédemment</h3>
+			<div class="trash col-md-2">
+				<span><i class="fa fa-trash"></i></span>
+			</div>
+		</div>
+		<div class="box-content">
+			<div class="message">
+				<span class="mail-open col-md-1">Mail icon</span>
+				<div class="subject col-md-6">RE: Liste hôtels Paris</div>
+				<div class="from col-md-4">De : Best Western Londres <br /> le 08/03 à 08h00</div>
+				<form action="" class="delete-message col-md-1">
+					<input type="checkbox" name="delete-message" />
+				</form>
+			</div>
+		</div>
+	</section>
 </div>
