20c73f6d2174af0912de217def3e7371776689d8
ThomasG17 <t.gauret17@gmail.com>
2016-03-09 16:32:13 +0100

    login v2

---
+++ controllers/UserController.class.php
@@ -0,0 +1,51 @@
+<?php
+class UserController extends basesql{
+
+  public function indexAction($args){
+    $v = new view("login");
+    $v->assign("mesargs", $args);
+  } 
+
+  public function loginAction($args){
+    $v = new view("login");
+    $v->assign("mesargs", $args);
+  }
+
+  public function AuthUserAction(){
+    $v = new view("login");
+
+    if(empty($_POST['email']))
+    {
+      $args['error']= "erreur: email non renseigné";
+    }
+
+    if(empty($_POST['password']))
+    {
+      $args['error']= "error: password non renseigné";
+    }
+
+    if(empty($_POST['email']) && empty($_POST['password'])){
+      $args['error'] = "error: email & password non renseignés";
+    }
+    $user_login = $this->getUserLogin($_POST['email'],$_POST['password']);
+
+    if(!$user_login == false){
+      $_SESSION['login_user'] = array(
+                                  'firstname' => $user_login['firstname'],
+                                  'lastname' => $user_login['lastname'],
+                                  'mail' => $user_login['mail'],
+                                  'is_admin' => $user_login['is_admin'],
+                                  'fonction' => $user_login['fonction']
+                                );
+    }else{
+      $args['error'] = "error: combinaison mot de passe email invalide";
+    }
+    $v->assign("mesargs", $args);
+    header("location: ../");
+  }
+
+  public function logoutAction(){
+    session_destroy();
+    header("location: login"); 
+  }
+}
---
+++ views/login.php
@@ -0,0 +1,21 @@
+
+<div id="main">
+  <div class="header-brand col-md-4 col-md-offset-4 col-xs-12 col-sm-8 col-sm-offset-2"></div>
+  <div id="login" class="col-md-4 col-md-offset-4 col-xs-12 col-sm-8 col-sm-offset-2">
+    <h2 class="connection col-md-12 col-xs-12 col-sm-12">Connexion</h2>
+    <form action="/user/AuthUser" method="post">
+      <div class="input-group">
+        <div class="input-group-addon">
+          <img src="../../public/images/bonhomme.png" class="user-img"/>
+        </div>
+        <input id="email" class="email col-md-12 col-xs-12 col-sm-12" name="email" placeholder="Email" type="email">
+      </div>
+      <input id="password" class=" password col-md-12 col-xs-12 col-sm-12"name="password" placeholder="Mot de passe" type="password">
+      <input name="submit" class="login col-md-12 col-xs-12 col-sm-12 btn btn-primary" type="submit" value=" Connexion ">
+      <a href="#" class="login_link col-md-12 col-xs-12 col-sm-12">Mot de passe oublié?</a>
+      <?php if(!empty($mesargs)){ ?>
+        <span><?php echo $mesargs['error']; ?></span>
+      <?php } ?>
+    </form>
+  </div>
+</div>
