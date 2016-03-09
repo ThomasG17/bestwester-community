58a286f8aad638b4c27d7ff49e83f76cc9f29c58
Amélie <amliebarre@gmail.com>
2016-03-09 12:09:17 +0100

    finalisation de la page d'accueil (index.php)

--- views/index.php
+++ views/index.php
@@ -1,20 +1,43 @@
 <div id="messages">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
+		<div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
 			<div class="message">
-				<div class="message-content">
-					<img src="" class="hostel-picture col-md-3" alt="Photo de l'hôtel" />
-					<section class="hostel-informations">
-						<p>Hôtel Paris - Montmartre</p>
-						<p><i class="fa fa-clock-o"></i> le 08.03.2016</p>
-					</section>
-					<section class="hostel-news">
+				<img src="/public/images/hostel-picture.jpg" class="hostel-picture col-md-3" alt="Photo de l'hôtel" />
+				<section class="hostel-informations col-md-9 col-md-offset-2 col-sm-12 col-sm-offset-3 col-xs-12 col-xs-offset-3">
+					<p>Hôtel Paris - Montmartre</p>
+					<p><i class="fa fa-clock-o"></i> le 08.03.2016</p>
+				</section>
+				<section class="hostel-news col-md-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae urna a dui
+						fermentum hendrerit at vel leo. Donec auctor convallis lorem sed pulvinar. Suspendisse
+						eget sem porta, iaculis tortor ut, pellentesque erat. Phasellus sed quam ante. Vestibulum
+						aliquam risus nunc, dictum sagittis mauris laoreet nec. In mattis at lectus vitae 
+						tincidunt. Quisque eget pulvinar odio. Duis ac diam fringilla ex cursus efficitur eu nec lorem. 
+						Etiam consectetur pharetra erat nec eleifend. Nam posuere porttitor metus mollis pellentesque. 
+						Sed a rhoncus urna. Donec rutrum libero vel massa tempus dapibus. In lobortis odio lacus, in 
+						laoreet metus cursus fermentum. Nam in vestibulum leo.</p>
+				</section>
 
-					</section>
-					<section class="comments">
-
-					</section>
-				</div>
+				<!-- Comments section -->
+				<section id="comments">
+					<div class="comments-section">
+						<h3>Derniers commentaires</h3>
+						<div class="comment">
+							<section class="hostel-response-informations">
+								<p>Hôtel Marais - Bastille</p>
+								<p><i class="fa fa-clock-o"></i> le 08.03.2016</p>
+							</section>
+							<section class="news-comment">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae urna a dui
+						fermentum hendrerit at vel leo.</p>
+							</section>
+						</div>
+					</div>
+					<form method="post" id="comment-form">
+						<textarea type="post" placeholder="Votre commentaire..."></textarea>
+						<input type="submit" value="Envoyer" />
+					</form>
+				</section>
 			</div>
 		</div>
 	</div>
--- views/layout.php
+++ views/layout.php
@@ -9,6 +9,7 @@
 
     <link rel="stylesheet" href="/public/css/main.css">
     <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="/public/css/media-queries.css">
     <link rel="stylesheet" href="/public/css/tpl/home.css">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -21,32 +22,36 @@
 
 <body>
     <header id="header">
-            <div class="header-brand"></div>
-            <div class="user-informations">
-                <p class="name">Nom prénom</p>
-                <p class="statut">Fonction</p>
-                <div class="hostel">
-                    <p class="hostel-name">Hôtel Marais Bastille</p>
-                    <p class="hostel-adress">36 Bd Richard Lenoir <br /> 75011 Paris</p>
-                </div>
+        <h1 class="header-brand"></h1>
+        <div class="user-informations">
+            <p class="name">Nom prénom</p>
+            <p class="statut">Fonction</p>
+            <div class="hostel">
+                <p class="hostel-name">Hôtel Marais Bastille</p>
+                <p class="hostel-adress">36 Bd Richard Lenoir <br /> 75011 Paris</p>
             </div>
+        </div>
 
-            <nav id="main-nav">
-                <ul>
-                    <li><a href="javascript:void(0);" title="Accueil">Accueil</a></li>
-                    <li><a href="javascript:void(0);" title="Messagerie">Messagerie</a></li>
-                    <li><a href="javascript:void(0);" title="Planning">Planning</a></li>
-                    <li><a href="javascript:void(0);" title="Réservations">Réservations</a></li>
-                    <li><a href="javascript:void(0);" title="Hôtels">Hôtels</a></li>
-                </ul>
-            </nav>
+        <nav id="main-nav">
+            <ul>
+                <li><a href="javascript:void(0);" title="Accueil"><i class="fa fa-home"></i> Accueil</a></li>
+                <li><a href="javascript:void(0);" title="Messagerie"><i class="fa fa-envelope"></i> Messagerie</a></li>
+                <li><a href="javascript:void(0);" title="Planning"><i class="fa fa-calendar"></i> Planning</a></li>
+                <li><a href="javascript:void(0);" title="Hôtels"><i class="fa fa-bed"></i> Hôtels</a></li>
+            </ul>
+        </nav>
+
+        <nav id="responsive-main-nav">
+            <ul>
+                <li><a href="javascript:void(0);" title="Accueil"><i class="fa fa-home"></i></a></li>
+                <li><a href="javascript:void(0);" title="Messagerie"><i class="fa fa-envelope"></i></a></li>
+                <li><a href="javascript:void(0);" title="Planning"><i class="fa fa-calendar"></i></a></li>
+                <li><a href="javascript:void(0);" title="Planning"><i class="fa fa-bed"></i></a></li>
+            </ul>
+        </nav>
     </header>
 
-<<<<<<< HEAD
     <div class="content">
-=======
-    <div id="contenu">
->>>>>>> cbdfd511b3f6565714c7680f7f8a1663da748dd1
         <div class="container-fluid">
             <div class="row">
                 <form method="search" id="search">
