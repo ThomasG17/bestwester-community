f51be36dc987816e92b69097f2342fd8e8ea3e03
Amélie <amliebarre@gmail.com>
2016-03-11 00:35:19 +0100

    modification vue hostels (map)

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,26 @@ $(document).ready(function() {
 
 	// Map
 	$("#spain").click(function() {
-  		alert("ok");
+  		$("#hostels-map-content").hide();
+  		$("#spain-map-content").show();
+	});
+
+	$("#salobrena").click(function() {
+		$("#hotel-salobrena").show();
+		$("#hotel-madrid").hide("slow");
+		$("#hotel-barcelone").hide("slow");
+	});
+
+	$("#barcelone").click(function() {
+		$("#hotel-barcelone").show();
+		$("#hotel-madrid").hide("slow");
+		$("#hotel-salobrena").hide("slow");
+	});
+
+	$("#madrid").click(function() {
+		$("#hotel-madrid").show();
+		$("#hotel-barcelone").hide("slow");
+		$("#hotel-salobrena").hide("slow");
 	});
 
 	// Calendar
