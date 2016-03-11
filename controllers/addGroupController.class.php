30662ea0108263a208e5e51c3a901317c2695069
ChiTai <chitai95@hotmail.fr>
2016-03-11 13:32:15 +0100

    Revert "Ajout de membre dans un groupe"
    This reverts commit 68d860140f668df6fdc307ffc9c49ea1ade48cbb.

--- controllers/addGroupController.class.php
+++ controllers/addGroupController.class.php
@@ -12,15 +12,14 @@ class addGroupController{
     }
 
     public function insertAction($args){
+        var_dump($_SESSION["login_user"]);
         $id = -1;
-        $id_user = $_SESSION['id'];
+        $id_user = $_SESSION['login_user']['user_id'];
         $group = $_POST['Groupes'];
 
         $id_group = $this->getGroup($group)['id'];
         $addUser = new user_GroupModel($id, $id_user, $id_group);
         $addUser->save();
-
-        header("Location: /index");
     }
 
     public function getGroupAction($groupName) {
@@ -35,7 +34,6 @@ class addGroupController{
         $v = new view("createGroup");
         $v->assign("messargs", $args);
     }
-
     public function insertGroupAction($args) {
         var_dump($_POST['group-title']);
         $id = -1;
@@ -43,32 +41,5 @@ class addGroupController{
         $insertGroup = new groupModel($id, $name);
         $insertGroup->save();
 
-        header("Location: /index");
-    }
-
-    public function addMemberGroupAction($args) {
-        $groupListObj = new groupModel();
-        $groups = $groupListObj->getAll(true);
-
-        $userListObj = new userModel();
-        $users = $userListObj->getAll(true);
-
-        $v = new view("addMemberGroup");
-        $v->assign("messargs", $args);
-        $v->assign("users", $users);
-        $v->assign("groups", $groups);
-
-    }
-
-    public function insertMemberGroupAction($args) {
-        $id = -1;
-        $id_user = $_POST['User'];
-        $id_group = $_POST['Groupe'];
-        var_dump($_POST);
-
-        $insertGroup = new user_GroupModel($id, $id_user, $id_group);
-        $insertGroup->save();
-
-        header("Location: /index");
     }
 }
--- models/userModel.class.php
+++ models/userModel.class.php
@@ -7,22 +7,16 @@ class userModel extends basesql {
 	protected $mail;
 	protected $fonction;
 	protected $is_admin;
-	protected $id_group;
-	protected $password;
 
-
-	public function __construct($id=-1, $lastname="", $firstname="", $mail="", $fonction="", $is_admin="", $id_group="", $password=""){
-
+	public function __construct($id=0, $lastname="", $firstname="", $mail="", $fonction="", $is_admin=0) {
 		parent::__construct();
 
 		$this->setId($id);
-		$this->setLastname($lastname);
-		$this->setFirstname($firstname);
+		$this->setLastName($lastname);
+		$this->setFirstName($firstname);
 		$this->setMail($mail);
 		$this->setFonction($fonction);
-		$this->setIs_Admin($is_admin);
-		$this->setId_Group($id_group);
-		$this->setPassword($password);
+		$this->setIsAdmin($is_admin);
 	}
 
 
@@ -32,27 +26,24 @@ class userModel extends basesql {
 		$this->id = $id;
 	}
 
-	public function setLastname($lastname){
-		$this->id_user = trim($lastname);
+	public function setLastName($lastname){
+		$this->lastname = $lastname;
 	}
 
-	public function setFirstname($firstname){
-		$this->id_group = trim($firstname);
+	public function setFirstName($firstname){
+		$this->firstname = $firstname;
 	}
+
 	public function setMail($mail){
-		$this->mail = trim($mail);
+		$this->mail = $mail;
 	}
+
 	public function setFonction($fonction){
-		$this->fonction = trim($fonction);
+		$this->fonction = $fonction;
 	}
-	public function setIs_Admin($is_admin){
-		$this->is_admin = trim($is_admin);
-	}
-	public function setId_Group($id_group){
-		$this->id_group = trim($id_group);
-	}
-	public function setPassword($password){
-		$this->password = trim($password);
+
+	public function setIsAdmin($is_admin){
+		$this->is_admin = $is_admin;
 	}
 
 
@@ -61,27 +52,24 @@ class userModel extends basesql {
 		return $this->id;
 	}
 
-	public function getLastname(){
+	public function getLastName(){
 		return $this->lastname;
 	}
 
-	public function getFirstname(){
+	public function getFirstName(){
 		return $this->firstname;
 	}
+
 	public function getMail(){
 		return $this->mail;
 	}
+
 	public function getFonction(){
 		return $this->fonction;
 	}
-	public function getIs_Admin(){
+
+	public function getIsAdmin(){
 		return $this->is_admin;
-	}
-	public function getId_Group(){
-		return $this->id_group;
-	}
-	public function getPassword(){
-		return $this->password;
 	}
 
 }
